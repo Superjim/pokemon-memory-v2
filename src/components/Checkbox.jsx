61d75bb1822006fcbc98244709c71e561b06b72c
Jim Jenkinson <jamesjenkinson10@gmail.com>
2023-01-30 22:47:15 +0000

    made checkbox status more clear

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -2,7 +2,13 @@ import React from "react";
 
 function Checkbox({ label, id, range, image, checked, onChange }) {
   return (
-    <div className="checkbox" onClick={onChange}>
+    <div
+      className="checkbox"
+      onClick={onChange}
+      style={{
+        border: checked ? "2px solid green" : "2px solid red",
+      }}
+    >
       {label}
       <br />
       <input type="checkbox" id={id} checked={checked} onChange={onChange} />
