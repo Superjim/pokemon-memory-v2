ac5b56006656423ed60cd5a3186772197099f8a2
Superjim <jamesjenkinson10@gmail.com>
2023-01-31 16:14:28 +0000

    removed unnessecary or

--- src/components/DifficultyContainer.jsx
+++ src/components/DifficultyContainer.jsx
@@ -61,10 +61,8 @@ function DifficultyContainer({ setGameData }) {
         Get points by clicking on a Pokemon, but don't click on one more than
         once!
       </p>
-      {rangeDifficulty.length === 0 ? (
+      {rangeDifficulty.length === 0 && (
         <h3>Please select atleast one generation of Pokemon</h3>
-      ) : (
-        ""
       )}
       {userDifficulty < 2 ? (
         <button disabled>
